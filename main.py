a08a8fdcafd8dee5c1bfc3e071ab5c71ec5ea7e2
SShivshaurya <singh.Saurabh368@Gmail.com>
2021-07-20 13:10:59 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+import numpy as np
+from flask import Flask, request,jsonify,render_template,url_for
+import pickle
+
+app = Flask(__name__)
+model = pickle.load(open('randomForestRegressor.pkl','rb'))
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+    print(prediction[0])
+
+    return render_template('home.html',prediction_text='AQI for Jaipur {}'.format(prediction))
+
+@app.route('/predict_api',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls through request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
+
+    output = prediction[0]
+    return jsonify(output)
+
+if __name__ == '__main__':
+    app.run(debug=True)
